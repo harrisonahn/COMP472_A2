d0c13f64e49af3830682762050dbd95cf695f8eb
harrisonahn <harrisonahn27@gmail.com>
2020-11-09 23:13:08 -0500

    Reads File Inputs

--- A*.py
+++ A*.py
@@ -1,1 +1,20 @@
+import os
+
 print("Search Algorithm A*")
+
+#reading contents of file
+file = open('samplePuzzles.txt', 'r')
+puzzle = file.readline()
+print(puzzle)
+
+currentPuzzle = ['0', '0']
+currentPuzzle.append(puzzle)
+print(currentPuzzle)
+
+
+
+
+
+
+
+
